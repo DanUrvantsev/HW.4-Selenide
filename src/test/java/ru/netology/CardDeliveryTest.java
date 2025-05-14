89bc41d385bf24f2c472c13b5d3cb3393cdf3e23
DanUrvantsev <laorant8@gmail.com>
2025-05-14 22:16:52 +0700

    selenide test fix

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -10,7 +10,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
     private String generateDate(int daysToAdd, String pattern) {
@@ -28,7 +27,7 @@ public class CardDeliveryTest {
         $("[data-test-id='phone'] input").setValue("+79211234567");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
-        $(".notification__content]").should(Condition.visible, Duration.ofSeconds(15))
-                .should(Condition.text("Встреча успешно забронирована на" + deliveryDate));
+        $(".notification__content").should(Condition.visible, Duration.ofSeconds(15))
+                .should(Condition.text("Встреча успешно забронирована на " + deliveryDate));
     }
 }
